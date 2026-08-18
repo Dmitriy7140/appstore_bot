0a98127d78052a27d0785caaee4cb0ef9eeb86b5
Dmitriy7140 <vereshagin7140@gmail.com>
2026-08-18 12:51:37 +0300

    Switch Mini App to test domain

--- keyboards/webapp_buttons.py
+++ keyboards/webapp_buttons.py
@@ -3,7 +3,7 @@ from urllib.parse import parse_qsl, urlencode, urlsplit, urlunsplit
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 
 
-WEB_APP_URL = "https://public-inky-sigma.vercel.app"
+WEB_APP_URL = "https://testamos.2pay.money"
 
 
 def mini_app_url(start_param: str) -> str:
